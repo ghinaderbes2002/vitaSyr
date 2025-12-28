8f3d5da2e11ad5736a5bc544c7cec0196f66899b
ghinaderbes2002 <ghinaderbes@gmail.com>
2025-12-28 13:52:06 +0300

    update ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // Enable standalone output for Docker
+  output: "standalone", // Enable standalone output for Docker
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
       },
       {
-        protocol: 'http',
-        hostname: '217.76.53.136',
-        port: '3010',
+        protocol: "http",
+        hostname: "159.198.37.52",
+        port: "3010",
       },
     ],
     unoptimized: true, // تعطيل التحسين للصور المحلية
